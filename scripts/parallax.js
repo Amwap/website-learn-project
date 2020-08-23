a9ec9ae3813039f79bcb30698b1b3b1373250fa4
Илья Коновалов <Amwap>
2020-08-23 04:43:42 +0300

    пока всё работает как надо кроме параллакса

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -1,9 +1,10 @@
-$('body').on('mousemove',(e) =>{
-    const x = e.pageX / $(window).width();
-    const y = e.pagey / $(window).height();
-    console.log(x)
+
+$('body').on('mousemove',(k) =>{
+    const x = k.pageX / $(window).width();
+    const y = k.pagey / $(window).height();
+    console.log(x);
     $('.mouse_parallax_background').css(
         'transform',
-        `translate(-${x*30}px, -${y*30}px)`
+        'translate(-'+ x*30 + 'px,-' + y*30 + 'px)'
     );
 });
