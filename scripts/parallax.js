e804db343b91db9a17dba06ec8209f6efea30ffc
Илья Коновалов <Amwap>
2020-08-25 19:16:00 +0300

    working wersion minimal

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -1,10 +1,11 @@
 
-$('body').on('mousemove',(k) =>{
+$('.mouse_parallax').on('mousemove',(k) =>{
     const x = k.pageX / $(window).width();
     const y = k.pagey / $(window).height();
     console.log(x);
     $('.mouse_parallax_background').css(
         'transform',
-        'translate(-'+ x*30 + 'px,-' + y*30 + 'px)'
+        `translate(-${x*30}px, -${y*30}px)`
+
     );
 });
